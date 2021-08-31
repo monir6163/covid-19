4dd755a0ab27248dfa49734094940dd9b80e61a6
netstermonir <monirhossain6163@gmail.com>
2021-08-31 15:02:54 +0600

    add spinner

--- js/covid.js
+++ js/covid.js
@@ -1,29 +1,84 @@
 // loadcovid-19 fetch api 
+const resultCovid = document.getElementById('covid-result');
+const diffMessage = document.getElementById('error-message');
+const poster = document.getElementById("poster");
+const spinner = `<div class = "text-center mt-3">
+<div class="spinner-border text-white" role="status" id ="load">
+<span class="visually-hidden">Loading...</span>
+</div>
+</div>`;
+let inputValue;
 const loadcovid = async () => {
-    const res = await fetch('https://api.covid19api.com/summary');
-    const data = await res.json();
-    displayCovid(data.Countries)
+    const searchField = document.getElementById('search-country');
+    const searchResult = searchField.value;
+    searchField.value = "";
+    if(searchResult == ""){
+        diffMessage.innerHTML = `<div class=" w-100 mx-auto alert alert-danger alert-dismissible fade show" role="alert">
+  <strong>Hello 3rd Person!</strong> Please Enter Your FuckUp Name.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>`;
+    }
+    else{
+        resultCovid.innerHTML = spinner;
+        const res = await fetch(`https://api.covid19api.com/total/country/${searchResult}`);
+        inputValue = searchResult;
+        const data = await res.json();
+        displayData(data);
+    }
 }
-loadcovid();
-const displayCovid = Countries => {
-    const covidResult = document.getElementById('covid-result');
-    Countries.forEach(country => {
-        const div = document.createElement('div');
-        div.classList.add('col-md-4');
-        div.innerHTML = `<div class="card h-100">
-                        <h5 class="card-header text-center text-muted">Country Information</h5>
-                        <div class="card-body">
-                            <h5 class="card-title">Country: <span class = "text-white bg-success p-1 rounded fs-6">${country.Country}</span></h5>
-                            <h5 class="card-title">CountryCode: ${country.CountryCode}</h5>
-                            <h5 class="card-title">NewConfirmed: ${country.NewConfirmed}</h5>
-                            <h5 class="card-title">NewDeaths: ${country.NewDeaths}</h5>
-                            <h5 class="card-title">NewRecovered: ${country.NewRecovered}</h5>
-                            <h5 class="card-title">TotalConfirmed: ${country.TotalConfirmed}</h5>
-                            <h5 class="card-title">TotalDeaths: ${country.TotalDeaths}</h5>
-                            <h5 class="card-title">TotalRecovered: ${country.TotalRecovered}</h5>
-                        </div>
-                        <div class="card-footer text-muted">Date: ${country.Date}</div>
-                    </div>`;
-        covidResult.appendChild(div); 
-    });
+const displayData = async data => {
+    const resultCovid = document.getElementById('covid-result');
+    if (Array.isArray(data) == false) {
+    diffMessage.innerHTML = `<div class=" w-100 mx-auto alert alert-danger alert-dismissible fade show" role="alert">
+  <strong>${inputValue}</strong>  This Country Name Not Found.
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>`;
+  } else if (Array.isArray(data)) {
+    resultCovid.innerHTML = "";
+    const country = data.reverse()[0];
+    const countryName = country.Country;
+    const totalCase = country.Confirmed;
+    const totalDeath = country.Deaths;
+    const totalRecoverd = country.Active;
+    const resultDiv = document.createElement("div");
+    resultDiv.classList.add("result");
+    resultDiv.innerHTML = `
+   <h3>Result:<span> ${inputValue}</span></h3>
+   <h3>Country:<span>${countryName}</span></h3>
+   <h3>Total Cases:<span>${totalCase}</span></h3>
+   <h3>Total Deaths:<span class="text-danger">${totalDeath}</span></h3>
+   <h3>Recovered:<span>${totalRecoverd}</span></h3>
+   `;
+    resultCovid.appendChild(resultDiv);
+  }
+  document.getElementById('load').style.display= "none";
 }
+
+
+
+
+// const displayData = (data) => {
+//   const container = document.getElementById("container");
+
+//   if (Array.isArray(data) == false) {
+//     diffMessage.innerText = `Sorry ${inputValue} is not found.`;
+//   } else if (Array.isArray(data)) {
+//     diffMessage.innerText = `Result: ${inputValue}`;
+//     container.innerHTML = "";
+//     const country = data.reverse()[0];
+//     const countryName = country.Country;
+//     const totalCase = country.Confirmed;
+//     const totalDeath = country.Deaths;
+//     const totalRecoverd = country.Active;
+
+//     const resultDiv = document.createElement("div");
+//     resultDiv.classList.add("result");
+//     resultDiv.innerHTML = `
+//    <h3>Country:<span>${countryName}</span></h3>
+//    <h3>Total Cases:<span>${totalCase}</span></h3>
+//    <h3>Total Deaths:<span class="text-danger">${totalDeath}</span></h3>
+//    <h3>Recovered:<span>${totalRecoverd}</span></h3>
+//    `;
+//     container.appendChild(resultDiv);
+//   }
+// };
