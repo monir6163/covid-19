83d6050575c76f46b5a9bfcc1e8119681d08da98
netstermonir <monirhossain6163@gmail.com>
2021-08-31 15:10:07 +0600

    spinner add

--- js/covid.js
+++ js/covid.js
@@ -53,32 +53,3 @@ const displayData = async data => {
   }
   document.getElementById('load').style.display= "none";
 }
-
-
-
-
-// const displayData = (data) => {
-//   const container = document.getElementById("container");
-
-//   if (Array.isArray(data) == false) {
-//     diffMessage.innerText = `Sorry ${inputValue} is not found.`;
-//   } else if (Array.isArray(data)) {
-//     diffMessage.innerText = `Result: ${inputValue}`;
-//     container.innerHTML = "";
-//     const country = data.reverse()[0];
-//     const countryName = country.Country;
-//     const totalCase = country.Confirmed;
-//     const totalDeath = country.Deaths;
-//     const totalRecoverd = country.Active;
-
-//     const resultDiv = document.createElement("div");
-//     resultDiv.classList.add("result");
-//     resultDiv.innerHTML = `
-//    <h3>Country:<span>${countryName}</span></h3>
-//    <h3>Total Cases:<span>${totalCase}</span></h3>
-//    <h3>Total Deaths:<span class="text-danger">${totalDeath}</span></h3>
-//    <h3>Recovered:<span>${totalRecoverd}</span></h3>
-//    `;
-//     container.appendChild(resultDiv);
-//   }
-// };
